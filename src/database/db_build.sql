e425e75fdb94914e7420acd767ce72cec98f52f3
nataphilips <fnd.natalia@gmail.com>
2020-01-23 11:07:11 +0300

    Returned the barder working schedule to db and fixed queries

--- src/database/db_build.sql
+++ src/database/db_build.sql
@@ -1,5 +1,5 @@
 BEGIN;
-  DROP TABLE IF EXISTS users, articles, bookings, services CASCADE;
+  DROP TABLE IF EXISTS bookings, services, working_schedule CASCADE;
 
   CREATE TABLE services (
     service_id SERIAL PRIMARY KEY,
@@ -8,6 +8,7 @@ BEGIN;
     service_cost INTEGER NOT NULL DEFAULT 0,
     service_duration INTEGER NOT NULL DEFAULT 30
   );
+
 
   CREATE TABLE bookings (
     booking_id SERIAL PRIMARY KEY,
@@ -20,10 +21,20 @@ BEGIN;
     FOREIGN KEY (service_id) REFERENCES services(service_id)
   );
 
+  CREATE TABLE working_schedule (
+    schedule_id SERIAL PRIMARY KEY,
+    week_day INTEGER NOT NULL,
+    start_hr VARCHAR(100) NOT NULL,
+    end_hr VARCHAR(100) NOT NULL
+  );
+
   INSERT INTO services (service_name, description, service_cost, service_duration)
     VALUES ('Regular haircut','Regular haircut <br /> shampoo', 40, 30);
 
   INSERT INTO bookings (booking_date, booking_time, customer_email, customer_name, customer_phone, service_id)
     VALUES ('20191124', '0900', 'ghj@g.com ', 'Itay', '0544791675', 1);
+
+  INSERT INTO working_schedule (week_day, start_hr, end_hr)
+    VALUES (0, '0900', '2100');
 
 COMMIT;
--- src/queries/adminHoursQueries.js
+++ src/queries/adminHoursQueries.js
@@ -1,20 +1,28 @@
 const databaseConnection = require("../database/db_connection.js");
 
 const getallhours = day => {
-  return databaseConnection.query(`SELECT * FROM schedule where day=$1`, [day]);
+  return databaseConnection.query(
+    `SELECT * FROM working_schedule where week_day=$1`,
+    [day]
+  );
 };
 
-const modifyworkingday = (data) => {
-  const values = data.list.reduce((acc, row) => { return acc += `('${row.day}',' ${row.start_time}', '${row.end_time}'),` }, '').slice(0, -1);
-  const result = databaseConnection.query(`DELETE FROM schedule WHERE day=$1;`, [data.day]).catch(err => console.log(err));
+const modifyworkingday = data => {
+  const values = data.list
+    .reduce((acc, row) => {
+      return (acc += `('${row.day}',' ${row.start_time}', '${row.end_time}'),`);
+    }, "")
+    .slice(0, -1);
+  const result = databaseConnection
+    .query(`DELETE FROM working_schedule WHERE week_day=$1;`, [data.day])
+    .catch(err => console.log(err));
   if (data.list.length) {
-    return databaseConnection.query("INSERT INTO schedule (day,start_time,end_time) VALUES " + [values]);
+    return databaseConnection.query(
+      "INSERT INTO working_schedule (week_day, start_hr, end_hr) VALUES " +
+        [values]
+    );
   }
   return result;
 };
 
 module.exports = { modifyworkingday, getallhours };
-
-
-
-
